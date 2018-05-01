a454fbbff7a26ce6b019c91805e1f0db074f5642
TucemaJI <bidi75518@gmail.com>
2018-05-01 19:51:40 +0300

    Player for game

---
+++ Player/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player
+{
+    public class Player
+    {
+        private string Name { get; set; }
+
+        protected short hp = 300;
+        protected short mp = 600;
+        protected short stamina = 700;
+    }
+}
